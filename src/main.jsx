8e9041c3c0654a84d6eb08f193e10481c88d92e7
Kolesanencore <kolesanencore@gmail.com>
2023-10-11 00:19:04 +0300

    added global styles, images, and dependencies

--- src/main.jsx
+++ src/main.jsx
@@ -1,10 +1,15 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import App from './App.jsx'
-import './index.css'
+import React from 'react';
+import ReactDOM from 'react-dom/client';
+
+import App from './App.jsx';
+
+import GlobalStyles from './styles/GlobalStyles';
+
+import 'modern-normalize/modern-normalize.css';
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
+    <GlobalStyles />
     <App />
   </React.StrictMode>,
-)
+);
